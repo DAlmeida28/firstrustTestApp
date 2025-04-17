7858504f42c0de902f2ed27cd367f4332f6b7dbd
Daniel Almeida <almeida.danny@gmail.com>
2025-04-16 21:29:52 -0500

    add QR code creation dependcies and added to html

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use yew::prelude::*;
+extern crate qrc;
+use qrc::*; 
 
 #[function_component(App)]
 fn app() -> Html {
+    let qrcode = QRCode::from_string("test".to_string());
     html! {
         <>
             <title>{"I love rust!"}</title>
             <h1>{"Hello World!"}</h1>
+            <button onclick={Callback::from(|_| ())}>{"create QR code"}</button>
         </>
     }
 }
